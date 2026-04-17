3e4cb737170c63af1306fed3da02e1f53388ece0
Jesuslegui <leguizamojesusda@gmail.com>
2026-04-16 19:31:11 -0500

    Feat: Creacion de Bios y los pod slots
    agregamos al modelo main board el Bios model y los slots pod de la tarjeta madre

--- JiCpu/Modelos/Mainboard.cs
+++ JiCpu/Modelos/Mainboard.cs
@@ -9,10 +9,12 @@ namespace JiCpu.Modelos
     {
         public required string Marca { get; set; }
         public string? Modelo { get; set; }
+        public string? BiosModel {get; set;}
         public int Port { get; set; }
         public string? Socket { get; set; }
         public string? Chipset { get; set; }
         public string? Bus { get; set; }
+        public int POD_slots {get; set;}
 
     }
 }
